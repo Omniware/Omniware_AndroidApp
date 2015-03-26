9fd7b1346cf19d17c436298f1fb5c38999d1f711
jishthedish <jishnut@gmail.com>
2015-03-26 20:51:15 +0530

    code with splash and listview
    listview

---
+++ MyApplication/app/src/main/java/com/omni/splash/omnisplash/MainActivity.java
@@ -0,0 +1,30 @@
+package com.omni.splash.omnisplash;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
+        setListAdapter(adapter);
+    }
+
+    }
